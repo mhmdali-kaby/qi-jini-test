a654a4618c31ac47b823c67eb2e96c0a99f8b4c9
Mohammed Ali <mohammedaliraeed1@gmail.com>
2025-01-15 20:37:20 +0300

    swipeable

--- src/components/page-wrapper/index.tsx
+++ src/components/page-wrapper/index.tsx
@@ -1,15 +1,16 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {AnimatePresence, motion} from 'framer-motion';
 import Header from "../header";
 import Nav from "../nav";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import {AnimateContainer} from "./styled.ts";
 import {routesConfig} from "../../router/routes.ts";
+import {useSwipeable} from "react-swipeable";
 
 const pageVariants = {
-	initial: {x: '100vw', opacity: 2},
-	animate: {x: 0, opacity: 1},
-	exit: {x: '-100vw', opacity: 0},
+	initial: { x: '100%', opacity: 0.8, scale: 0.95 },
+	animate: { x: 0, opacity: 1, scale: 1 },
+	exit: { x: '-100%', opacity: 0.8, scale: 0.95 },
 };
 
 interface PageWrapperProps {
@@ -21,26 +22,24 @@ interface PageWrapperProps {
 
 const PageWrapper: React.FC<PageWrapperProps> = ({children}) => {
 	const location = useLocation();
+	const navigate = useNavigate();
 	const [routeConfig, setRouteConfig] = useState<{
 		withHeader?: boolean;
 		withNav?: boolean;
 		showHeaderIcon?: boolean;
 	}>({withHeader: true, withNav: true, showHeaderIcon: true});
 
-	const [isSwipeNavigation, setIsSwipeNavigation] = useState(false);
-	const previousPathname = useRef(location.pathname);
 
-	useEffect(() => {
-		const handlePopState = () => {
-			setIsSwipeNavigation(true);
-			setTimeout(() => setIsSwipeNavigation(false), 300); // Timeout matches animation duration
-		};
-
-		window.addEventListener('popstate', handlePopState);
-		return () => {
-			window.removeEventListener('popstate', handlePopState);
-		};
-	}, []);
+	const swipeHandlers = useSwipeable({
+		onSwipedLeft: () => {
+			navigate(1);
+		},
+		onSwipedRight: () => {
+			navigate(-1);
+		},
+		preventScrollOnSwipe: true,
+		trackTouch: true,
+	});
 
 	useEffect(() => {
 		const currentConfig = Object.keys(routesConfig).find(
@@ -52,18 +51,17 @@ const PageWrapper: React.FC<PageWrapperProps> = ({children}) => {
 		} else {
 			setRouteConfig(routesConfig[currentConfig]);
 		}
-		previousPathname.current = location.pathname;
 	}, [location]);
 
 
 	return (
 		<>
 			<Header showIcon={routeConfig.showHeaderIcon} show={routeConfig.withHeader}/>
-			<AnimateContainer>
+			<AnimateContainer {...swipeHandlers}>
 				<AnimatePresence mode="wait">
 					<motion.div
 						variants={pageVariants}
-						initial={isSwipeNavigation ? false : 'initial'}
+						initial={'initial'}
 						animate="animate"
 						exit="exit"
 						transition={{type: 'keyframes', duration: 0.1}}
